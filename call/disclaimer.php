2a407bad66bbbd0cc94f49286835636051a14105
Matt McCormick <mbmccormick@gmail.com>
2010-09-19 11:57:04 -0400

    Updated database settings.

--- call/disclaimer.php
+++ call/disclaimer.php
@@ -5,6 +5,7 @@
     echo "<Gather action='/call/record.php?number=$_POST[Digits]' method='POST' numDigits='1'>\n";
     echo "<Say voice='man'>Federal and State Laws require consent of both parties in order to record this call. To play an automated message to your party when they answer, press one. Otherwise, to do it yourself, please stay on the line.</Say>\n";
     echo "</Gather>\n";
-    echo "</Response>\n";
+    echo "<Redirect>http://recordableapp.com/call/record.php?number=$_POST[Digits]</Redirect>\n";
+    echo "</Response>\n";    
 
 ?>
