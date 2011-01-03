eb9e82671ad13d831574e4445adcc3e9732bf181
Matt McCormick <mbmccormick@gmail.com>
2011-01-02 23:22:48 -0500

    Completely revamped entire website including styling, call flow, added text messaging, added jQuery error handlers, added jQuery back to top button.

--- call/answer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
-    echo "<Response>\n";
-    echo "<Say voice='man'>Hello, the person trying to reach you would like to record this phone call. To accept, please stay on the line. Otherwise, hang up now.</Say>\n";
-    echo "<Pause length='1' />";
-    echo "</Response>\n";
-
-?>
