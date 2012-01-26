4b70795ad49396d5b2e90e3b0021829003991406
Matt McCormick <mbmccormick@gmail.com>
2012-01-26 14:05:50 -0500

    Update views/common/purchase.php

--- views/common/purchase.php
+++ views/common/purchase.php
@@ -1,13 +1,7 @@
 <div class="row">
     <div class="span15">
-        <audio controls="controls" autoplay="autoplay">
-            <source src="<?=$conversation[recordingurl]?>.mp3" type="audio/mpeg" />
-            Your browser does not support audio playback.
-        </audio>
+        <p>To listen to this recording online, there is a one-time fee of <b>$<?=(0.35 + (ceil($conversation[duration] / 60) * 0.10))?></b>. This payment can be completed online and offers unlimited access to your recording.</p>
         <br />
-        <br />
-        <p>This conversation took place on <?=date("F j, Y", strtotime($conversation[createddate]))?> between <?=$caller?> and <?=$called?> and lasted <?=round($conversation[duration]/60)?> minutes.</p>
-        <br />
-        <a href="<?=option('base_uri')?>download/<?=$conversation[sessioncode]?>" target="_blank">Click here to download</a>
+        <a href="<?=option('base_uri')?>download/<?=$conversation[sessioncode]?>" target="_blank">Click here to purchase</a>
     </div>
 </div>
