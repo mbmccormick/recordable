7bc16af3ec3faad637c877a4904239ded54b2194
Matt McCormick <mbmccormick@gmail.com>
2012-01-26 14:10:27 -0500

    Adding payment view.

--- views/common/purchase.php
+++ views/common/purchase.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="span15">
-        <p>To listen to this recording online, there is a one-time fee of <b>$<?=(0.35 + (ceil($conversation[duration] / 60) * 0.10))?></b>. This payment can be completed online and offers unlimited access to your recording.</p>
+        <p>To purchase online access to this recording, click the link below. The total for this recording is <b>$<?=(0.35 + (ceil($conversation[duration] / 60) * 0.10))?></b>, a one-time payment that provides unlimited access to your recording.</p>
         <br />
         <a href="<?=option('base_uri')?>download/<?=$conversation[sessioncode]?>" target="_blank">Click here to purchase</a>
     </div>
